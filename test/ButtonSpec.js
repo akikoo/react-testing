a039e16abb87cfb317bd0621e97ad9bd5fb12e1f
Aki Karkkainen <etimiti@gmail.com>
2015-10-08 20:06:14 +0200

    Upgrade React to v0.14

--- test/ButtonSpec.js
+++ test/ButtonSpec.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var React = require('react/addons');
-var ReactTestUtils = React.addons.TestUtils;
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactTestUtils = require('react-addons-test-utils');
 var sinon = require('sinon');
 var expect = require('unexpected').clone();
 var ButtonComponent = require('../components/Button');
@@ -11,28 +12,28 @@ expect.installPlugin(require('unexpected-sinon'));
 describe('ButtonComponent', () => {
   it('should render a button element', () => {
     var instance = ReactTestUtils.renderIntoDocument(<ButtonComponent />);
-    expect(React.findDOMNode(instance).nodeName, 'to be', 'BUTTON');
+    expect(ReactDOM.findDOMNode(instance).nodeName, 'to be', 'BUTTON');
   });
 
   it('should have button class "btn"', () => {
     var instance = ReactTestUtils.renderIntoDocument(<ButtonComponent />);
-    expect(React.findDOMNode(instance).className, 'to contain', 'btn');
+    expect(ReactDOM.findDOMNode(instance).className, 'to contain', 'btn');
   });
 
   it('should be of type "button"', () => {
     var instance = ReactTestUtils.renderIntoDocument(<ButtonComponent />);
-    expect(React.findDOMNode(instance).getAttribute('type'), 'to be', 'button');
+    expect(ReactDOM.findDOMNode(instance).getAttribute('type'), 'to be', 'button');
   });
 
   it('should contain a value', () => {
     var instance = ReactTestUtils.renderIntoDocument(<ButtonComponent />);
-    expect(React.findDOMNode(instance).textContent, 'to be', 'Send');
+    expect(ReactDOM.findDOMNode(instance).textContent, 'to be', 'Send');
   });
 
   it('should call onClick callback', () => {
     var callback = sinon.spy();
     var instance = ReactTestUtils.renderIntoDocument(<ButtonComponent value="Send" onClick={callback} />);
-    var node = React.findDOMNode(instance);
+    var node = ReactDOM.findDOMNode(instance);
 
     ReactTestUtils.Simulate.click(node);
     expect(callback, 'was called');
