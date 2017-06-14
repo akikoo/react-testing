d4f8b92614ba86d980519cc7dd7f654d9a736c61
Aki Karkkainen <etimiti@gmail.com>
2017-06-14 09:30:19 +0200

    Use new jsdom syntax

--- test/utils/dom.js
+++ test/utils/dom.js
@@ -3,14 +3,14 @@
 var jsdom = require('jsdom');
 
 // setup the simplest document possible
-var doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+var doc = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
 
 // get the window object out of the document
-var win = doc.defaultView;
+var win = doc.window;
 
 // set globals for mocha that make access to document and window feel
 // natural in the test environment
-global.document = doc;
+global.document = win.document;
 global.window = win;
 
 // take all properties of the window object and also attach it to the
