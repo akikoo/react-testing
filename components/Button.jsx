5b974ec60a4c069f56acfa0b246339419cdd021c
Aki Karkkainen <etimiti@gmail.com>
2017-06-14 10:30:58 +0200

    Adapt syntax after React upgrade

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,32 +3,35 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
-var Button = React.createClass({
-  getDefaultProps() {
-    return {
-      type: 'button',
-      value: 'Send',
-      onClick: null
-    };
-  },
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
   handleClick(e) {
     if (this.props.onClick) {
       this.props.onClick(e);
     }
-  },
+  }
 
   render() {
     return (
       <button className="btn btn--primary" type="button" onClick={this.handleClick}>{this.props.value}</button>
     );
   }
-});
+};
 
 Button.propTypes = {
   type: PropTypes.string,
   value: PropTypes.string.isRequired,
   onClick: PropTypes.func
+};
+
+Button.defaultProps = {
+  type: 'button',
+  value: 'Send',
+  onClick: null
 };
 
 module.exports = Button;
